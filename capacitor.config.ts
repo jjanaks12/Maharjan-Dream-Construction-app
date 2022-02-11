4253204fbb19466a6c8ec76a268591c49614b91e
Janak Shrestha <jjanaks12@gmail.com>
2022-02-11 10:51:14 +0545

    app icon fixes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.maharjanc.app',
-  appName: 'MaharjanConstruction',
+  appName: 'Guthi',
   webDir: 'dist',
   bundledWebRuntime: false
 };
